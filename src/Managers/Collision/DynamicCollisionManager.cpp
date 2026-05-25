b1cb9ea6668a1c9ebb73601bceb621db4febdaa6
BingusEx <10480438+BingusEx@users.noreply.github.com>
2026-05-25 20:19:27 +0200

    Fix runtime colliders getting double scaled

--- src/Managers/Collision/DynamicCollisionManager.cpp
+++ src/Managers/Collision/DynamicCollisionManager.cpp
@@ -1,10 +1,51 @@
 #include "Managers/Collision/DynamicCollisionManager.hpp"
 
+namespace {
+
+	struct Unk58 {
+		uint64_t unk00;
+		uint32_t unk08;
+		uint32_t flags;
+		uint64_t unk10;
+		uint64_t unk18;
+		uint64_t unk20;
+		RE::NiAVObject* object;
+
+		inline bool CheckFlags() const {
+			return (((flags & 0x70000000) - 0x30000000) & 0xEFFFFFFF) == 0;
+		}
+	};
+
+	//Based on DCA's RE
+	bool HasRuntimeCollisions(RE::Actor* a_actor) {
+		if (!a_actor) return false;
+
+		RE::LOADED_REF_DATA* loadedData = a_actor->loadedData;
+		if (!loadedData) return false;
+
+		if (loadedData->unk60 && loadedData->unk58) {
+			if (auto pUnk58 = reinterpret_cast<Unk58**>(loadedData->unk58)) {
+				auto unk58 = *pUnk58;
+				if (unk58->CheckFlags()) {
+					return true;
+				}
+			}
+		}
+		return false;
+	}
+}
+
+
 namespace GTS {
 
 	void DynamicCollisionManager::CreateInstance(RE::Actor* a_actor) {
-		WriteLock lock(MapLock);
-		ControllerMap.try_emplace(a_actor->GetCharController(), std::make_shared<DynamicCollisionController>(a_actor->GetHandle()));
+
+		bool HasRuntimeCol = HasRuntimeCollisions(a_actor);
+
+		{
+			WriteLock lock(MapLock);
+			ControllerMap.try_emplace(a_actor->GetCharController(), std::make_shared<DynamicCollisionController>(a_actor->GetHandle(), HasRuntimeCol));
+		}
 	}
 
 	void DynamicCollisionManager::DestroyInstance(RE::bhkCharacterController* a_controller) {
